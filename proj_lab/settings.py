80554ca0bf99a320d7e1df6209108cac402e30a4
ck-wobbuffet <393972871@qq.com>
2020-07-29 10:00:19 +0800

    update base.html and sql

--- proj_lab/settings.py
+++ proj_lab/settings.py
@@ -136,7 +136,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
